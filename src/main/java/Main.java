3f33cc6d9160105fa8fac1a3de0ea712b7fe8a85
Snehasish Roy <snehasishroy39@gmail.com>
2022-12-04 01:47:44 +0530

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException, IOException {
+        Scraper scraper = new Scraper();
+        scraper.setup();
+    }
+}
