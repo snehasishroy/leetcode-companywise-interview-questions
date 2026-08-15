fba277fc720b81a0e083226bc62139e420d1cefd
snehasishroy <snehasishroy39@gmail.com>
2026-08-16 00:12:09 +0530

    Update problems as of 12 July

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws InterruptedException, IOException {
+    public static void main(String[] args) throws InterruptedException {
         Scraper scraper = new Scraper();
         scraper.setup();
     }
